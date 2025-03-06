a40f3441dafc1b2fc17e446b0059832bc20deb1e
Kervin Parungao <159155995+KervinP17@users.noreply.github.com>
2025-03-06 02:25:15 -0500

    Début serveur tcp

--- src/main/java/ca/cal/tp2/Main.java
+++ src/main/java/ca/cal/tp2/Main.java
@@ -1,7 +1,34 @@
 package ca.cal.tp2;
 
+import ca.cal.tp2.modele.Utilisateur;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.h2.tools.Server;
+
+import java.sql.SQLException;
+import jakarta.persistence.EntityManager;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // Votre script qui utilise votre API ici
+        //
+
+        final Server tcpServer = Server.createTcpServer(
+                "-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+        System.out.println("Tcp server start: " + tcpServer.start());
+        System.out.println(tcpServer.getStatus() + " " +
+                tcpServer.getPort());
+        System.out.println("jdbc:h2:tcp://localhost:9092/mem:bibliotheque");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("bibliotheque");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+
+        em.getTransaction().commit();
+        em.close();
+
+
+
     }
 }
