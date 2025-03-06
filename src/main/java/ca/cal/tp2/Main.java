72f6cdf4a0533786ff03c8c31b7ea51f0bf50d88
Kervin Parungao <159155995+KervinP17@users.noreply.github.com>
2025-03-06 14:51:49 -0500

    refactor Serveur tcp dans un autre class

--- src/main/java/ca/cal/tp2/Main.java
+++ src/main/java/ca/cal/tp2/Main.java
@@ -1,6 +1,8 @@
 package ca.cal.tp2;
 
 import ca.cal.tp2.modele.Utilisateur;
+import ca.cal.tp2.repository.UtilisateurRepositoryJPA;
+import ca.cal.tp2.service.EmprunteurService;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import org.h2.tools.Server;
@@ -13,20 +15,7 @@ public class Main {
         // Votre script qui utilise votre API ici
         //
 
-        final Server tcpServer = Server.createTcpServer(
-                "-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
-        System.out.println("Tcp server start: " + tcpServer.start());
-        System.out.println(tcpServer.getStatus() + " " +
-                tcpServer.getPort());
-        System.out.println("jdbc:h2:tcp://localhost:9092/mem:bibliotheque");
-
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("bibliotheque");
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-
-
-        em.getTransaction().commit();
-        em.close();
+        TcpServer.createServer();
 
 
 
